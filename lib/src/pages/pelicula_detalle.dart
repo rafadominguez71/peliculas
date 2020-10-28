0042f777b000df25f7c19b96bb28721f4145fee4
Rafael Dominguez <rafael.dominguez.mora@iesjandula.es>
2020-10-28 17:40:59 +0100

    Descripcion peliculas

--- lib/src/pages/pelicula_detalle.dart
+++ lib/src/pages/pelicula_detalle.dart
@@ -6,10 +6,23 @@ class PeliculaDetalle extends StatelessWidget {
   Widget build(BuildContext context) {
     final Pelicula pelicula = ModalRoute.of(context).settings.arguments;
     return Scaffold(
-      body: CustomScrollView(
-        slivers: <Widget> [
-          _crearAppbar(pelicula),
-        ],
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget> [
+            _crearAppbar(pelicula),
+            SliverList(
+              delegate: SliverChildListDelegate(
+                [
+                  SizedBox(height:10.0),
+                  _posterTitulo(context, pelicula),
+                  _descripcion(pelicula),
+                ]
+ 
+              ),
+            )
+          ];
+        },
+        body: Text(''),
       ),
     );
   }
@@ -36,4 +49,47 @@ class PeliculaDetalle extends StatelessWidget {
       ),
     );
   }
+
+ Widget _posterTitulo(BuildContext context, Pelicula pelicula) {
+   return Container(
+     padding: EdgeInsets.symmetric(horizontal:20.0),
+     child: Row(
+       children: [
+         ClipRRect(
+           borderRadius: BorderRadius.circular(20.0),
+           child: Image(
+             image: NetworkImage(pelicula.getPosterImg()),
+             height: 150.0,
+           ),
+         ),
+         SizedBox(width: 20.0,),
+         Flexible(
+           child: Column(
+             crossAxisAlignment: CrossAxisAlignment.start,
+             children: [
+               Text(pelicula.title, style: Theme.of(context).textTheme.headline6,overflow: TextOverflow.ellipsis,),
+               Text(pelicula.originalTitle, style: Theme.of(context).textTheme.subtitle1,overflow: TextOverflow.ellipsis,),
+               Row(
+                 children: [
+                   Icon(Icons.star_border),
+                   Text(pelicula.voteAverage.toString(),style: Theme.of(context).textTheme.subtitle1)
+                 ],
+               )
+             ],
+           ),
+         )
+       ],
+     ),
+   );
+ }
+
+ Widget _descripcion(Pelicula pelicula) {
+   return Container(
+     padding: EdgeInsets.symmetric(horizontal: 10.0, vertical:20.0),
+     child: Text(
+       pelicula.overview,
+       textAlign: TextAlign.justify,
+     ),
+   );
+ }
 }
--- lib/src/providers/peliculas_provider.dart
+++ lib/src/providers/peliculas_provider.dart
@@ -1,64 +1,60 @@
-import 'dart:async';
-import 'dart:convert';
-
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
 import 'package:peliculas/src/models/pelicula_model.dart';
 
-class PeliculasProvider{
-  String _apikey      ='9a926064eb0bfcfa2f6a1f946e6de57b';
-  String _url         ='api.themoviedb.org';
-  String _language    ='es-ES';
 
-  int _popularesPage=0;
-  bool _cargando = false;
+class PeliculasProvider {
+
+  ***REMOVED***
+  String _url      = 'api.themoviedb.org';
+  String _language = 'es-ES';
+
+  int _popularesPage = 0;
+  bool _cargando     = false;
 
   List<Pelicula> _populares = new List();
 
   final _popularesStreamController = StreamController<List<Pelicula>>.broadcast();
+  Function(List<Pelicula>) get popularesSink => _popularesStreamController.sink.add;
+  Stream<List<Pelicula>> get popularesStream => _popularesStreamController.stream;
 
-Function(List<Pelicula>) get popularesSink =>_popularesStreamController.sink.add;
-Stream<List<Pelicula>> get popularesStream => _popularesStreamController.stream;
 
- 
-  void disposeStreams(){
+  void disposeStreams() {
     _popularesStreamController?.close();
   }
 
-  Future<List<Pelicula>> _procesarRespuesta(Uri url) async{
-    final resp=await http.get(url);
+
+  Future<List<Pelicula>> _procesarRespuesta(Uri url) async {
+  
+    final resp = await http.get( url );
     final decodedData = json.decode(resp.body);
     final peliculas = new Peliculas.fromJsonList(decodedData['results']);
     return peliculas.items;
   }
-
-  Future <List<Pelicula>>getEnCines() async{
-    final url = Uri.http(_url, '3/movie/now_playing',{
-      'api_key' : _apikey,
-      'language' : _language,
+  Future<List<Pelicula>> getEnCines() async {
+    final url = Uri.https(_url, '3/movie/now_playing', {
+      'api_key'  : _apikey,
+      'language' : _language
     });
     return await _procesarRespuesta(url);
   }
- 
-  Future <List<Pelicula>> getPopulares()async{
-    if(_cargando) return [];
-    _cargando=true;
 
+  Future<List<Pelicula>> getPopulares() async {
+    
+    if ( _cargando ) return [];
+    _cargando = true;
     _popularesPage++;
-
-
-    final url = Uri.http(_url, '3/movie/popular',{
-      'api_key': _apikey,
-      'languaje' : _language,
-      'page':_popularesPage.toString()
+    final url = Uri.https(_url, '3/movie/popular', {
+      'api_key'  : _apikey,
+      'language' : _language,
+      'page'     : _popularesPage.toString()
     });
-
     final resp = await _procesarRespuesta(url);
 
     _populares.addAll(resp);
     popularesSink( _populares );
-    _cargando=false;
+    _cargando = false;
     return resp;
-    
   }
- 
 }
